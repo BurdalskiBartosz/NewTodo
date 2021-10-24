5038f4af9c23142bfeeefa8c8dd7b721ab29346b
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2021-10-24 21:39:54 +0200

    feat(front): Add base for todo form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import "./scss/index.scss";
 import { TodoList } from "./js/index.js";
 
-const application = new TodoList();
-application.init();
+class Application {
+	form;
+	todoList;
+
+	init() {
+		localStorage.removeItem("todos");
+		this.todoList = new TodoList();
+		this.todoList.init();
+		this.form = document.querySelector(".form");
+	}
+
+	addListeners() {
+		this.form.addEventListener("submit", this.handleSubmitForm.bind(this));
+	}
+
+	handleSubmitForm(e) {
+		e.preventDefault();
+		const values = this.getValuesFromForm();
+		this.addToList(values);
+	}
+
+	getValuesFromForm() {
+		const items = [...this.form.elements];
+		const values = {};
+		for (let item of items) {
+			if (!item.name) break;
+			values[item.name] = item.value;
+		}
+		return values;
+	}
+
+	addToList(values) {
+		this.todoList.addTodo(values);
+	}
+}
+const app = new Application();
+app.init();
+app.addListeners();
