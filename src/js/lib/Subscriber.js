0cfb9fdbe74fa2389691cf9593f32faf07e1989b
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2021-10-27 22:10:56 +0200

    feat(front): Add simple implementation of Observer pattern

---
+++ src/js/lib/Subscriber.js
@@ -0,0 +1,19 @@
+class Subscriber {
+	constructor() {
+		this.subscribers = [];
+	}
+
+	registerSubscriber(subscriber) {
+		this.subscribers.push(subscriber);
+	}
+
+	unregisterSubscriber(subscriber) {
+		this.subscribers = this.subscribers.filter((obs) => obs !== subscriber);
+	}
+
+	notifySubscribers(data) {
+		this.subscribers.forEach((obs) => obs.notify(data));
+	}
+}
+
+export { Subscriber };
